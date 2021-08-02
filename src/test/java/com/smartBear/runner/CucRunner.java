ef67515a7ef2de161da4ed2fa2c7d41d43132258
Rasim Aghayev <rasim.ag1983@gmail.com>
2021-08-02 01:24:54 -0400

    new pom fayls nad plugins

--- src/test/java/com/smartBear/runner/CucRunner.java
+++ src/test/java/com/smartBear/runner/CucRunner.java
@@ -7,6 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
 
         features = "src/test/resources/features",
         glue = "com/smartBear/step_definitions",
